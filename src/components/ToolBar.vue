3f7c16223a24a13f2f916086f06cddcc7bcb8dd6
KUEUNJI <yoo128@naver.com>
2023-01-13 00:54:48 +0900

    힝

--- src/components/ToolBar.vue
+++ src/components/ToolBar.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="header">
-    <router-link to="/news">News뉴스</router-link> |
-    <router-link to="/ask">Ask애스크</router-link> |
-    <router-link to="/jobs">Jobs잡스sss</router-link>
+    <router-link to="/news">News</router-link> |
+    <router-link to="/ask">Ask</router-link> |
+    <router-link to="/jobs">Jobs</router-link>
   </div>
 </template>
 
