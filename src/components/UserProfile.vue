57e23d683a7390de53103c331ce538b6ea962b7a
KUEUNJI <yoo128@naver.com>
2023-02-13 23:32:57 +0900

    2가지 데이터 처리 흐름 비교부터~~

---
+++ src/components/UserProfile.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="user-container">
+    <div>
+      <i class="fa-solid fa-user"></i>
+    </div>
+    <div class="user-description">
+      <div>
+        {{ info.id }}
+      </div>
+      <!-- <router-link v-bind:to="`/user/${userInfo.user}`">
+        {{ userInfo.id }}
+      </router-link> -->
+      <div class="time">
+        {{ info.created }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    info: Object
+  }
+}
+</script>
+
+<style scoped>
+.user-container {
+  display: flex;
+  align-items: center;
+  padding: 0.5rem;
+}
+.fa-user { 
+  font-size: 2.5rem;;
+}
+.user-description {
+  padding-left: 8px;
+}
+.time {
+  font-size: 0.7rem;
+}
+</style>
