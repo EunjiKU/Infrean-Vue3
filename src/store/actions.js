d620bec7819445ceb3b25188b9151414959d1dad
KUEUNJI <yoo128@naver.com>
2023-02-20 01:53:34 +0900

    완성~git add .! 만약 리셋하려면 여기부터

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,26 +1,51 @@
 import {
+  fetchNewsList,
+  fetchJobsList,
+  fetchAskList,
   fetchList,
   fetchUserInfo,
   fetchCommentItem
 } from '../api/index.js';
 
 export default {
+  FETCH_NEWS(context) {
+    return fetchNewsList()
+    .then(response => {
+      context.commit('SET_NEWS', response.data);
+      return response;
+    })
+    .catch(error => console.log(error))
+  },
+  FETCH_JOBS({ commit }) {
+    return fetchJobsList()
+    .then(({ data }) => {
+      commit('SET_JOBS', data)
+    })
+    .catch(error => console.log(error))
+  },
+  FETCH_ASK({ commit }) {
+    return fetchAskList()
+    .then(({ data }) => {
+      commit('SET_ASK', data)
+    })
+    .catch(error => console.log(error))
+  },
   FETCH_USER({ commit }, name) {
-    fetchUserInfo(name)
+    return fetchUserInfo(name)
     .then(({ data }) => {
       commit('SET_USER', data)
     })
     .catch(error => console.log(error))
   },
   FETCH_ITEM({ commit }, id) {
-    fetchCommentItem(id)
+    return fetchCommentItem(id)
     .then(({ data }) => {
       commit('SET_ITEM', data)
     })
     .catch(error => console.log(error))
   },
   FETCH_LIST({ commit }, pageName) {
-    fetchList(pageName)
+    return fetchList(pageName)
       .then(({ data }) => commit('SET_LIST', data))
       .catch(error => console.log(error));
   }
